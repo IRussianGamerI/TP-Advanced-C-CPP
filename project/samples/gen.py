43ad1206a446f2fcceb562fc0b2f81dd73b456cb
Лев Барабанщиков <levbara1@gmail.com>
2022-04-02 03:33:26 +0300

    fix: gen.py

--- project/samples/gen.py
+++ project/samples/gen.py
@@ -6,6 +6,7 @@ if len(sys.argv) == 5:
     f.write(sys.argv[1] + '\n')
     for i in range(int(sys.argv[1])):
         f.write(str(random.randint(int(sys.argv[2]), int(sys.argv[3]))) + '\n')
+    f.close()
     print("Successfully written {} random values from {} to {} into {}".format(sys.argv[1], sys.argv[2], sys.argv[3],
                                                                                sys.argv[4]))
     print("Written size: {} megabytes".format(int(sys.argv[1]) * 4 / (2 ** 20)))
